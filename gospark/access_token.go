25d9dc358c871c2e169d0ba9bc95a3ff702af504
indradhanush <indradhanush.gupta@gmail.com>
2014-07-21 21:42:28 +0530

    Brought OAuthRequest and OAuthResponse inside access_token.go

--- gospark/access_token.go
+++ gospark/access_token.go
@@ -4,21 +4,79 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/url"
+	"strings"
 )
 
 // AccessTokenService is an entrypoint for any AccessToken related operation.
 type AccessTokenService struct {
-	Oreq       *OAuthRequest
+	OaRequest  *OAuthRequest
 	AToken     *AccessToken
 	ATokenList *AccessTokenList
 }
 
-func NewAccessTokenService() *AccessTokenService {
+func NewAccessTokenService(username, password string) *AccessTokenService {
 	aTokenService := &AccessTokenService{}
+	aTokenService.OaRequest = NewOAuthRequest(username, password)
 	aTokenService.AToken = NewAccessToken()
 	aTokenService.ATokenList = NewAccessTokenList()
 
 	return aTokenService
+}
+
+// OAuthRequest is a struct for the body of a request to get an access
+// token.
+type OAuthRequest struct {
+	GrantType string `json:"grant_type, omitempty"`
+	UserName  string `json:"username, omitoempty"`
+	Password  string `json:"password, omitempty"`
+}
+
+func NewOAuthRequest(username, password string) *OAuthRequest {
+	oaReq := &OAuthRequest{}
+	oaReq.GrantType = "password"
+	oaReq.UserName = username
+	oaReq.Password = password
+
+	return oaReq
+}
+
+type OAuthResponse struct {
+	AccessToken string `json:"access_token"`
+	TokenType   string `json:"token_type"`
+	ExpiresIn   int64  `json:"expires_in"`
+}
+
+// Get returns an AccessToken in the form of a OAuthResponse object.
+func (s *AccessTokenService) GetAccessToken() (*OAuthResponse, error) {
+
+	urlStr := GetCompleteEndpointUrl(&APIUrl{BaseUrl: BaseUrl,
+		Endpoint: "/oauth/token"})
+
+	form := url.Values{}
+	form.Set("grant_type", s.OaRequest.GrantType)
+	form.Set("username", s.OaRequest.UserName)
+	form.Set("password", s.OaRequest.Password)
+
+	req, err := http.NewRequest("POST", urlStr, strings.NewReader(form.Encode()))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.SetBasicAuth(BasicAuthId, BasicAuthPassword)
+	if err != nil {
+		return nil, err
+	}
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	oauthResp := &OAuthResponse{}
+	err = json.NewDecoder(resp.Body).Decode(oauthResp)
+	if err != nil {
+		return nil, err
+	}
+
+	return oauthResp, nil
 }
 
 type AccessToken struct {
--- main.go
+++ main.go
@@ -6,20 +6,19 @@ import (
 )
 
 func main() {
-	oauthReq := &gospark.OAuthRequest{"password", gospark.UserName,
-		gospark.Password}
 
-	oauthToken, _ := oauthReq.Get()
-	fmt.Println(oauthToken.AccessToken)
-	fmt.Println(oauthToken.TokenType)
-	fmt.Println(oauthToken.ExpiresIn)
-
-	aTokenService := gospark.NewAccessTokenService()
+	aTokenService := gospark.NewAccessTokenService(gospark.UserName,
+		gospark.Password)
 	aTokenService.ListAllAccessTokens()
 	fmt.Println(aTokenService.ATokenList)
 
 	for _, token := range aTokenService.ATokenList.Tokens {
 		fmt.Println(token.Token, token.ExpiresAt, token.Client)
 	}
+
+	oauthToken, _ := aTokenService.GetAccessToken()
+	fmt.Println(oauthToken.AccessToken)
+	fmt.Println(oauthToken.TokenType)
+	fmt.Println(oauthToken.ExpiresIn)
 
 }
